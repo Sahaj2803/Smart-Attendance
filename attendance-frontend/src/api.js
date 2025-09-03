21b76f8ce3ed38c98a925e8cd2458148dfcd42cc
Sahaj2803 <sahajpatel2803@gmail.com>
2025-09-03 13:27:09 +0530

    Update api.js

--- attendance-frontend/src/api.js
+++ attendance-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-attendance-api-jibv.onrender.com/api",
+    baseURL: `${process.env.REACT_APP_API_BASE}/api`,
   headers: {
     "Content-Type": "application/json",
   },
