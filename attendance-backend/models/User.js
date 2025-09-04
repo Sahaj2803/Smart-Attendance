6173ed4df10517dcae9d3d23486a1d215a36ef2f
Sahaj2803 <sahajpatel2803@gmail.com>
2025-09-04 09:51:13 +0530

    Update User.js

--- attendance-backend/models/User.js
+++ attendance-backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
-    role: { type: String, enum: ["student", "faculty"], default: "student" }
+    role: { type: String, enum: ["student", "faculty", "admin"], default: "student" }
 });
 
 module.exports = mongoose.model("User", userSchema);
